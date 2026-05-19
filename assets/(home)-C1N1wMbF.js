e4dbe856d114ef8fc08a74a72406a345a8120cb2
masquevil <masquevil@users.noreply.github.com>
2026-05-19 01:04:08 +0000

    deploy: 336726c68c90c733936087b9998b3895f2ec55f3

---
+++ assets/(home)-C1N1wMbF.js
@@ -0,0 +1,32 @@
+import{k as e,m as t,p as n,s as r,t as i,u as a,y as o}from"./_plugin-vue_export-helper-CSk66ROA.js";import{t as s}from"./MarkdownRenderer-DZPMntfD.js";import{t as c}from"./PageAside-B7V3fgGo.js";var l=`# 侠小然的奇怪人生档案
+
+## 退役程序员
+
+## TRPG 跑团 KPL
+
+## 居家调酒师
+
+## 动画游戏宅
+
+（自动追番；家庭影院；电脑游戏；合作游戏；主机；桌游）
+
+## 痛车驾驶员
+
+## 床车露营实习生
+
+## 轻度极客玩家
+
+（家用服务器；3D打印；全屋智能）
+
+## 一年一度的旅者
+
+## 兴趣使然的摄影师
+
+## 摆烂理财师
+
+## 奶牛猫铲屎官
+
+## 叠加态终结者
+
+（未来的可能性，比如编曲等）
+`,u={class:`page`},d={class:`container`},f=i(t({__name:`index`,setup(t){return(t,i)=>(o(),a(`main`,u,[r(`div`,d,[n(s,{content:e(l)},null,8,[`content`])]),n(c,{content:e(l)},null,8,[`content`])]))}}),[[`__scopeId`,`data-v-f6fa0f5f`]]);export{f as default};
